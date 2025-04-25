09ef907fb853783431f45333fcc620bd06e29c4e
maxshlain <maxshlain@gmail.com>
2025-04-25 10:36:24 +0300

    print diagnostic info

--- src/AbstractSchnitzel/Program.cs
+++ src/AbstractSchnitzel/Program.cs
@@ -1,3 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+// print full path to current application
+Console.WriteLine("Appcontext BaseDirectory: " 
+                  + System.AppContext.BaseDirectory);
+
+// print working directory
+Console.WriteLine("Working directory: " 
+    + System.IO.Directory.GetCurrentDirectory());
+
+// print all command-line arguments
+Console.WriteLine("Command line arguments:");
+foreach (var arg in Environment.GetCommandLineArgs())
+{
+    Console.WriteLine($"  {arg}");
+}
+
